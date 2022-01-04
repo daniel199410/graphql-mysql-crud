940bca88b257cf1f0487156c39ca18b76e876149
dcatano <dcatanor@unal.edu.co>
2022-01-04 11:18:59 -0500

    MySql connection

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-import express from 'express';
-import {graphqlHTTP} from 'express-graphql';
-import {schema} from "./schema";
+import app from "./app";
+import {connectDB} from "./db";
 
-const app = express();
-app.use('/graphql', graphqlHTTP({
-    graphiql: true,
-    schema,
-}));
-app.listen(3000);
+async function main() {
+    try {
+        await connectDB()
+        app.listen(3000);
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+main().then(() => console.log('Server started'));
