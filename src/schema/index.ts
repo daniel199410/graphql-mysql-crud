60b74fb01eae59afa5ce8a0be9231bd671346d6d
dcatano <dcatanor@unal.edu.co>
2022-01-04 14:49:53 -0500

    Find user by id

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,13 +1,14 @@
 import {GraphQLSchema, GraphQLObjectType} from 'graphql';
 import {Greeting} from "./queries/greeting";
 import {CREATE_USER} from "./mutations/user";
-import {GET_ALL_USERS} from "./queries/user";
+import {GET_ALL_USERS, GET_USER} from "./queries/user";
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQuery',
     fields: {
         greeting: Greeting,
         getAllUsers: GET_ALL_USERS,
+        getUser: GET_USER,
     }
 })
 
--- src/schema/queries/user.ts
+++ src/schema/queries/user.ts
@@ -1,8 +1,14 @@
 import {User} from "../../entities/user";
 import {UserType} from "../typeDefs/user";
-import {GraphQLList} from "graphql";
+import {GraphQLID, GraphQLList} from "graphql";
 
 export const GET_ALL_USERS = {
     type: new GraphQLList(UserType),
     resolve: async () => await User.find()
+}
+
+export const GET_USER = {
+    type: UserType,
+    args: { id: { type: GraphQLID }},
+    resolve: async (_: any, args: any) => await User.findOne(args.id)
 }
